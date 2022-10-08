bd3b02949065017eecf0af97b235d13781268e71
Rostislav Rabiec <rosta.rabiec@icloud.com>
2022-10-08 18:19:22 +0200

    Vylepšeno

--- projekt_1.py
+++ projekt_1.py
@@ -52,7 +52,8 @@ are found in multiple limestone layers, which lie some
 represent several varieties of perch, as well as
 other freshwater genera and herring similar to those
 in modern oceans. Other fish such as paddlefish,
-garpike and stingray are also present.'''
+garpike and stingray are also present. The example that
+ends with number 1000'''
 ]
 
 # výběr textu k analýze
@@ -60,6 +61,14 @@ text_enter = int(input("Enter a number of text btw. 1 and 3 to select: "))
 
 if 1 <= text_enter <= 3:
     selected_text = TEXTS[text_enter - 1]
+    
+    # vyházet tečky, čárky a další nepísmenka a nečíslice, mezery a spol. nechat.
+    cleaned_text = ""
+    for character in selected_text:
+        if character.isalnum() or character.isspace():
+            cleaned_text += character
+    selected_text = cleaned_text
+
     print(line)
 
     # počet slov ve vybraném textu 
@@ -69,23 +78,24 @@ if 1 <= text_enter <= 3:
 
     # počet slov začínajících velkým písmenem
     title_case_words = []
-    for words in all_words:    
-        if words.istitle():
-            title_case_words.append(words)
-    print(f"There are {len(title_case_words)} titlecase words.") 
 
     # počet slov psaných velkými písmeny
     upper_words = [] 
-    for words in all_words:
-         if words.isupper() and words.isalpha():
-            upper_words.append(words)
-    print(f"There are {len(upper_words)} uppercase words.") 
 
     # počet slov psaných malými písmeny
     low_words = []
-    for words in all_words:
-          if words.islower():
+
+    for words in all_words:    
+        if words.istitle():
+            title_case_words.append(words)
+    
+        elif words.isupper() and words.isalpha():
+            upper_words.append(words)
+
+        elif words.islower():
               low_words.append(words)
+    print(f"There are {len(title_case_words)} titlecase words.") 
+    print(f"There are {len(upper_words)} uppercase words.")          
     print(f"There are {len(low_words)} lowercase words.")
 
 
