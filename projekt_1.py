98089956125823b1f92e780a90c02cfb609739e7
Rostislav Rabiec <rosta.rabiec@icloud.com>
2022-11-01 17:22:47 +0100

    Druhá oprava

--- projekt_1.py
+++ projekt_1.py
@@ -60,61 +60,63 @@ text_enter = int(input("Enter a number of text btw. 1 and 3 to select: "))
 
 if 1 <= text_enter <= 3:
     selected_text = TEXTS[text_enter - 1]
-    print(line)
-
-    # vyházet tečky, čárky a další nepísmenka a nečíslice, mezery a spol. nechat.
-    cleaned_text = ""
-    for character in selected_text:
-        if character.isalnum() or character.isspace():
-            cleaned_text += character
-    selected_text = cleaned_text
-
-    print(line)
-    
-    # počet slov ve vybraném textu 
-    all_words = selected_text.split()
-    
-    print(f"There are {len(all_words)} words in selected text.") 
-
-    # počet slov začínajících velkým písmenem
-    title_case_words = []
-
-    # počet slov psaných velkými písmeny
-    upper_words = [] 
-
-    # počet slov psaných malými písmeny
-    low_words = []
-
-    for words in all_words:    
-        if words.istitle():
-            title_case_words.append(words)
-    
-        elif words.isupper() and words.isalpha():
-            upper_words.append(words)
-
-        elif words.islower():
-              low_words.append(words)
-    print(f"There are {len(title_case_words)} titlecase words.") 
-    print(f"There are {len(upper_words)} uppercase words.")          
-    print(f"There are {len(low_words)} lowercase words.")
-
-    # počet čísel(ne cifer)
-    all_numbers = []
-    for num in all_words:
-         if num.isdigit():
-            all_numbers.append(num)
-    print(f"There are {len(all_numbers)} numeric strings.")
-
-    # suma všech čísel (ne cifer) v textu
-    total = 0
-    for sum in all_numbers:
-        total = total + int(sum) 
-    print(f"The sum of all the numbers is {total}.", line, sep="\n")
 
 # ohlášení, pokud byl zadáno špatné číslo textu
 else:
     print("Wrong number of text. Bye..")
     quit() 
+
+selected_text = TEXTS[text_enter - 1]
+print(line)
+
+
+# vyházet tečky, čárky a další nepísmenka a nečíslice, mezery a spol. nechat.
+cleaned_text = ""
+for character in selected_text:
+    if character.isalnum() or character.isspace():
+        cleaned_text += character
+    selected_text = cleaned_text
+
+print(line)
+    
+# počet slov ve vybraném textu 
+all_words = selected_text.split()
+print(f"There are {len(all_words)} words in selected text.") 
+
+# počet slov začínajících velkým písmenem
+title_case_words = []
+
+# počet slov psaných velkými písmeny
+upper_words = [] 
+
+# počet slov psaných malými písmeny
+low_words = []
+
+for words in all_words:    
+    if words.istitle():
+        title_case_words.append(words)
+    
+    elif words.isupper() and words.isalpha():
+        upper_words.append(words)
+
+    elif words.islower():
+            low_words.append(words)
+print(f"There are {len(title_case_words)} titlecase words.") 
+print(f"There are {len(upper_words)} uppercase words.")          
+print(f"There are {len(low_words)} lowercase words.")
+
+# počet čísel(ne cifer)
+all_numbers = []
+for num in all_words:
+        if num.isdigit():
+            all_numbers.append(num)
+print(f"There are {len(all_numbers)} numeric strings.")
+
+# suma všech čísel (ne cifer) v textu
+total = 0
+for sum in all_numbers:
+    total = total + int(sum) 
+print(f"The sum of all the numbers is {total}.", line, sep="\n") 
 
 # zobrazení jednoduchého sloupcového grafu, který ukazuje četnost různých délek slov
 
