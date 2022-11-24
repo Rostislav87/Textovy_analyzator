7c245e737d8fb8207094d880c3821790335a87ed
Rostislav Rabiec <rosta.rabiec@icloud.com>
2022-11-24 14:25:31 +0100

    Vytvoření modulu texts.py, který obsahuje všechny tři texty k analýze.

--- projekt_1.py
+++ projekt_1.py
@@ -5,6 +5,8 @@ author: Rostislav Rabiec
 email: rosta.rabiec@icloud.com
 discord: Rostislav R.#9305
 ''' 
+from texts import TEXTS
+
 # registrovaní uživatelé a hesla
 users = {
         "bob": "123",
@@ -25,35 +27,6 @@ if users.get(username) == password:
 else:
     print("Access denied. Wrong password or unregistered user.")
     quit()
-
-     # texty k analýze
-TEXTS = ['''
-Situated about 10 miles west of Kemmerer,
-Fossil Butte is a ruggedly impressive
-topographic feature that rises sharply
-some 1000 feet above Twin Creek Valley
-to an elevation of more than 7500 feet
-above sea level. The butte is located just
-north of US 30N and the Union Pacific Railroad,
-which traverse the valley. ''',
-'''At the base of Fossil Butte are the bright
-red, purple, yellow and gray beds of the Wasatch
-Formation. Eroded portions of these horizontal
-beds slope gradually upward from the valley floor
-and steepen abruptly. Overlying them and extending
-to the top of the butte are the much steeper
-buff-to-white beds of the Green River Formation,
-which are about 300 feet thick.''',
-'''The monument contains 8198 acres and protects
-a portion of the largest deposit of freshwater fish
-fossils in the world. The richest fossil fish deposits
-are found in multiple limestone layers, which lie some
-100 feet below the top of the butte. The fossils
-represent several varieties of perch, as well as
-other freshwater genera and herring similar to those
-in modern oceans. Other fish such as paddlefish,
-garpike and stingray are also present.'''
-]
 
 # výběr textu k analýze
 text_enter = int(input("Enter a number of text btw. 1 and 3 to select: "))
@@ -100,7 +73,7 @@ for words in all_words:
         upper_words.append(words)
 
     elif words.islower():
-        low_words.append(words)
+            low_words.append(words)
 print(f"There are {len(title_case_words)} titlecase words.") 
 print(f"There are {len(upper_words)} uppercase words.")          
 print(f"There are {len(low_words)} lowercase words.")
